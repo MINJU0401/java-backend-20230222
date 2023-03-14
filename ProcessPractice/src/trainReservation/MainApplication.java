2925eb95f5426ad8f2b7b51c850c1891057bea12
fuddms2 <fuddms2@gmail.com>
2023-03-14 12:03:32 +0900

    2023.03.14.

--- ProcessPractice/src/trainReservation/MainApplication.java
+++ ProcessPractice/src/trainReservation/MainApplication.java
@@ -1,5 +1,7 @@
 package trainReservation;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -14,12 +16,16 @@ public class MainApplication {
 	
 	private static List<Train> trains = new ArrayList<Train>();
 	private static List<Cost> costs = new ArrayList<Cost>();
+	
+	private static DateTimeFormatter timeformatter = DateTimeFormatter.ofPattern("HH:mm");
 
 	public static void main(String[] args) {
 		initData();
-		
+	
 		while (true) {			
 			GetTrainListDto dto = new GetTrainListDto();		//	변수명에 get같은 동사형태가 오면 안됨	
+			
+			LocalTime departureTime = null;
 			
 //			System.out.print("출발역 : ");			
 //			dto.setDepartureStation(scanner.nextLine());
@@ -30,13 +36,78 @@ public class MainApplication {
 //			System.out.print("인원 : ");
 //			dto.setNumberOfPeople(scanner.nextInt());		//	Dto 클래스에 넣어놓음//		
 			
-			if (dto.getArrivalStation().isBlank() ||dto.getDepartureStation().isBlank() ||dto.getDepartureTime().isBlank()) {
+			//	입력 검증
+			
+			if (dto.isEmpty()) {
 				System.out.println("모두 입력하세요.");
+				continue;
+			}	
+			
+			try {
+				departureTime = LocalTime.parse(dto.getDepartureTime(), timeformatter);
+			} 	catch (Exception exception) {
+				System.out.println("잘못된 시간입니다.");
 				continue;
 			}
 			
-			System.out.print(dto.toString());			
-			break;
+			if (dto.getNumberOfPeople() <= 0) {
+				System.out.println("잘못된 인원입니다.");
+				continue;
+			}
+			
+			if (dto.isEqualStation()) {
+				System.out.println("출발역과 도착역이 같습니다.");
+				continue;
+			}
+			
+			//	결과반환을 위한 변수 선언 및 리터럴 생성
+			
+			List<Train> possibleTrains = new ArrayList<>();
+			
+			for (Train train: trains) {
+				
+				List<StopStation> stopStations = train.getStopStations();
+				int sameStationIndex = -1;
+				
+				for (int stopStationIndex = 0; stopStationIndex < stopStations.size(); stopStationIndex++) {					
+					String stopStationName = stopStations.get(stopStationIndex).getStationName();
+					
+					if (!dto.isEqualDepartureStation(stopStationName)) {
+						continue;
+					}		
+					
+					LocalTime stationDepartureTime = LocalTime.parse(dto.getDepartureTime(), timeformatter);
+					
+					if (stationDepartureTime.isBefore(departureTime)) {
+						break;
+					}
+					
+					sameStationIndex = stopStationIndex;
+					break;					
+				}			
+				
+				if (sameStationIndex == -1) {
+					continue;
+				}
+				
+				for (int stopStationIndex = 0; stopStationIndex < stopStations.size(); stopStationIndex++) {
+					
+					String stationName = stopStations.get(stopStationIndex).getStationName();
+					
+					if (!dto.isEqualArrivalStation(stationName)) {
+						continue;
+					}
+					if (stopStationIndex <= sameStationIndex) {
+						break;
+					}
+					
+					possibleTrains.add(train);
+					break;
+				}
+				
+			}		
+			
+			System.out.println(possibleTrains.toString());
 		}				
 	}
 	
